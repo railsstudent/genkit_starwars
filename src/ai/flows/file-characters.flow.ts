290ecb4d7b50349f30e62f24da329e4c71d8d4ef
Connie Leung <cleung334@gmail.com>
2025-04-16 09:17:23 +0800

    refactor: flow calls ai.generateStream to stream response. generateStream also calls the tool

--- src/ai/flows/file-characters.flow.ts
+++ src/ai/flows/file-characters.flow.ts
@@ -1,16 +1,37 @@
+import { filmCharactersTool } from './../tools/file-characters.tool';
 import { filmCharacterResultsSchema, filmInputSchema } from '../../api';
 import { ai } from '../config';
-import { filmCharactersPrompt } from '../prompts/file-characters.prompt';
+import { promptConfig } from '../constants/safety-settings.constant';
 
 export const filmCharactersFlow = ai.defineFlow(
     {
       name: 'filmCharactersFlow',
       inputSchema: filmInputSchema,
       outputSchema: filmCharacterResultsSchema,
+      streamSchema: filmCharacterResultsSchema,
     },
-    async (input) => {
-      const { output } = await filmCharactersPrompt(input);
-    
+    async (input, { sendChunk }) => {
+      const response = await ai.generateStream({
+        system: `
+You are a Star Wars expert who can list the characters in Star Wars films. 
+You use the tool to perform a like search on film titles to find "${input.title}".
+If there is no film found, return an empty array. 
+If you do not know the answer, return an empty array and do not make up any characters or films.`,
+        prompt: `Who are in the Star Wars films where the title includes ${input.title}?`,
+        tools: [filmCharactersTool],
+        output: {
+            format: 'json',
+            schema: filmCharacterResultsSchema,
+        },
+        config: promptConfig,
+        model: 'googleai/gemini-2.0-flash',
+      })
+
+      for await (const chunk of response.stream) {
+        sendChunk((chunk as any).text);
+      }
+  
+      const { output } = await response.response;
       if (output == null) {
         throw new Error("Response doesn't satisfy schema.");
       }
