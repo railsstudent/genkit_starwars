151048ba1788cb4c2db32b4d85b1e2d305a22d58
Connie Leung <cleung334@gmail.com>
2025-04-16 12:19:22 +0800

    chore: delete commented lines

--- src/ai/index.ts
+++ src/ai/index.ts
@@ -1,7 +1,3 @@
 export * from './config';
-// export * from './prompts/file-characters.prompt';
-// export * from './prompts/people.prompt';
-// export * from './tools/file-characters.tool';
-// export * from './tools/people.tool';
 export * from './flows/file-characters.flow';
 export * from './flows/people.flow'
