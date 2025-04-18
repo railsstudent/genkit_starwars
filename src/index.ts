4b021d0b7cf3c52ad4cca9975e026be3e58472b9
Connie Leung <cleung334@gmail.com>
2025-04-18 23:03:56 +0800

    chore: delete commented codes

--- src/index.ts
+++ src/index.ts
@@ -8,19 +8,3 @@ startFlowServer({
     origin: '*',
   },
 });
-  
-
-// (async () => {
-//   try {
-//     // const output = await filmCharactersFlow({ title: 'sith' });
-//     // console.log('output', output);
-
-//     const output2 = await storyFlow({ name: 'ana' });
-//     console.log('output2', output2);
-
-//     // const output2 = await posterFlow({ name: 'ana' });
-//     // console.log('output2', output2);
-//   } catch (e) {
-//     console.error(e);
-//   }
-// })();
